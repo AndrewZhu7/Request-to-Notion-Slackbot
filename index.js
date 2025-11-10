fd8ea7fbb683fdc3382d4c18d854ea85d33f23b5
AndrewZhu7 <146140183+AndrewZhu7@users.noreply.github.com>
2025-11-10 16:45:01 -0500

    update for extra notion columns

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ app.command('/hubspot-ticket', async ({ ack, body, client }) => {
       submit: { type: 'plain_text', text: 'Submit' },
       close: { type: 'plain_text', text: 'Cancel' },
       blocks: [
+        //Take a request type
         {
           type: 'input',
           block_id: 'type_block',
@@ -36,12 +37,49 @@ app.command('/hubspot-ticket', async ({ ack, body, client }) => {
             ],
           },
         },
+        //Take the project name
         {
           type: 'input',
           block_id: 'title_block',
           label: { type: 'plain_text', text: 'Title' },
           element: { type: 'plain_text_input', action_id: 'title_input' },
         },
+        //Take the priority type
+        {
+          type: 'input',
+          block_id: 'prio_block',
+          label: { type: 'plain_text', text: 'Priority' },
+          element: {
+            type: 'static_select',
+            action_id: 'prio_select',
+            options: [
+              { text: { type: 'plain_text', text: 'High' }, value: 'High' },
+              { text: { type: 'plain_text', text: 'Medium' }, value: 'Medium' },
+              { text: { type: 'plain_text', text: 'Low' }, value: 'Low' },
+            ],
+          },
+        },
+        //Take the assigned Team type
+        {
+          type: 'input',
+          block_id: 'team_block',
+          label: { type: 'plain_text', text: 'Team' },
+          element: {
+            type: 'static_select',
+            action_id: 'team_select',
+            options: [
+              { text: { type: 'plain_text', text: 'Account Management' }, value: 'Account Management' },
+              { text: { type: 'plain_text', text: 'Human Resources' }, value: 'Human Resources' },
+              { text: { type: 'plain_text', text: 'Product Design' }, value: 'Product Design' },
+              { text: { type: 'plain_text', text: 'Business Development' }, value: 'Business Development' },
+            ],
+          },
+        },
+        //!!!!!!!Attach File NOT DONE!!!!!!!!!
+
+        //!!!!!!!Add Assignees NOT DONE!!!!!!!
+
+        //Take a description of the request
         {
           type: 'input',
           block_id: 'desc_block',
@@ -68,33 +106,63 @@ app.view('ticket_modal_submit', async ({ ack, body, view, client }) => {
   const type = values.type_block.type_select.selected_option.value;
   const title = values.title_block.title_input.value;
   const desc = values.desc_block.desc_input.value;
+  const prio = values.prio_block.prio_select.selected_option.value;
+  const team = values.team_block.team_select.selected_option.value;
 
   // --- Create new Notion page ---
   await notion.pages.create({
     parent: { data_source_id: process.env.NOTION_DB_ID },
     properties: {
-      Name: {
-        title: [{ text: { content: title } }],
+
+      'Project name': {
+        rich_text: [{ text: { content: title } }],
       },
+
+      //Select the type of ticket
       Type: {
         select: { name: type },
       },
+
+      //Status of the ticket is set to "Not Started"
       Status: {
-        select: { name: 'Backlog' }, // initial status column
+        select: { name: 'Not Started' },
       },
+
+      //Start date of ticket
+      'Start Date': {
+        select: { date: today() }
+      },
+
+      //Select the priority of the ticket (High, Low, Medium)
+      Priority: {
+        select: { name: prio },
+      },
+
+      //Select the assigned team
+      Team: {
+        select: { name: team },
+      },
+
+      //Description of task to be completed
+      Description: {
+        rich_text: [{ text: { content: desc } }],
+      },
+
+      //Created by tag
       CreatedBy: {
         rich_text: [{ text: { content: user } }],
       },
+      
     },
-    children: [
-      {
-        object: 'block',
-        type: 'paragraph',
-        paragraph: {
-          rich_text: [{ text: { content: desc } }],
-        },
-      },
-    ],
+    // children: [
+    //   {
+    //     object: 'block',
+    //     type: 'paragraph',
+    //     paragraph: {
+    //       rich_text: [{ text: { content: desc } }],
+    //     },
+    //   },
+    // ],
   });
 
     // --- Confirm in Slack ---
