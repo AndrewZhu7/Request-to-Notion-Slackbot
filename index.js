03ac539008cccb00b23f5433b7b8ec1259dac76f
Andrew Zhu <zhu.andre@northeastern.edu>
2026-01-20 18:19:26 -0500

    new updated dynamic fields

--- index.js
+++ index.js
@@ -1,16 +1,54 @@
-const {App} = require('@slack/bolt');
+const { App } = require('@slack/bolt');
+const { Client } = require('@notionhq/client');
 require('dotenv').config();
 
+// Initialize Slack app
 const app = new App({
-    token: process.env.SLACK_BOT_TOKEN,
-    signingSecret: process.env.SLACK_SIGNING_SECRET,
-    appToken: process.env.SLACK_APP_TOKEN,
-    socketMode: true,
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  appToken: process.env.SLACK_APP_TOKEN,
+  socketMode: true,
 });
 
-//"/hubspot-ticket" command call
+// Initialize Notion client
+const notion = new Client({
+  auth: process.env.NOTION_API_KEY,
+});
+
+// Helper function to get current date in ISO format
+function getTodayISO() {
+  return new Date().toISOString().split('T')[0];
+}
+
+// Helper function to fetch dynamic options from Notion database
+async function getNotionSelectOptions(propertyName) {
+  try {
+    const database = await notion.databases.retrieve({
+      database_id: process.env.NOTION_DB_ID,
+    });
+    
+    const property = database.properties[propertyName];
+    if (property && property.select && property.select.options) {
+      return property.select.options.map(option => ({
+        text: { type: 'plain_text', text: option.name },
+        value: option.name,
+      }));
+    }
+    return [];
+  } catch (error) {
+    console.error(`Error fetching ${propertyName} options:`, error);
+    return [];
+  }
+}
+
+// "/hubspot-ticket" command call
 app.command('/hubspot-ticket', async ({ ack, body, client }) => {
   await ack();
+
+  // Fetch dynamic options from Notion
+  const typeOptions = await getNotionSelectOptions('Type');
+  const priorityOptions = await getNotionSelectOptions('Priority');
+  const teamOptions = await getNotionSelectOptions('Team');
 
   // Open the modal
   await client.views.open({
@@ -22,7 +60,7 @@ app.command('/hubspot-ticket', async ({ ack, body, client }) => {
       submit: { type: 'plain_text', text: 'Submit' },
       close: { type: 'plain_text', text: 'Cancel' },
       blocks: [
-        //Take a request type
+        // Request Type
         {
           type: 'input',
           block_id: 'type_block',
@@ -30,21 +68,21 @@ app.command('/hubspot-ticket', async ({ ack, body, client }) => {
           element: {
             type: 'static_select',
             action_id: 'type_select',
-            options: [
+            options: typeOptions.length > 0 ? typeOptions : [
               { text: { type: 'plain_text', text: 'Bug' }, value: 'Bug' },
               { text: { type: 'plain_text', text: 'Feature' }, value: 'Feature' },
               { text: { type: 'plain_text', text: 'Change' }, value: 'Change' },
             ],
           },
         },
-        //Take the project name
+        // Title
         {
           type: 'input',
           block_id: 'title_block',
           label: { type: 'plain_text', text: 'Title' },
           element: { type: 'plain_text_input', action_id: 'title_input' },
         },
-        //Take the priority type
+        // Priority
         {
           type: 'input',
           block_id: 'prio_block',
@@ -52,14 +90,14 @@ app.command('/hubspot-ticket', async ({ ack, body, client }) => {
           element: {
             type: 'static_select',
             action_id: 'prio_select',
-            options: [
+            options: priorityOptions.length > 0 ? priorityOptions : [
               { text: { type: 'plain_text', text: 'High' }, value: 'High' },
               { text: { type: 'plain_text', text: 'Medium' }, value: 'Medium' },
               { text: { type: 'plain_text', text: 'Low' }, value: 'Low' },
             ],
           },
         },
-        //Take the assigned Team type
+        // Team
         {
           type: 'input',
           block_id: 'team_block',
@@ -67,7 +105,7 @@ app.command('/hubspot-ticket', async ({ ack, body, client }) => {
           element: {
             type: 'static_select',
             action_id: 'team_select',
-            options: [
+            options: teamOptions.length > 0 ? teamOptions : [
               { text: { type: 'plain_text', text: 'Account Management' }, value: 'Account Management' },
               { text: { type: 'plain_text', text: 'Human Resources' }, value: 'Human Resources' },
               { text: { type: 'plain_text', text: 'Product Design' }, value: 'Product Design' },
@@ -75,11 +113,7 @@ app.command('/hubspot-ticket', async ({ ack, body, client }) => {
             ],
           },
         },
-        //!!!!!!!Attach File NOT DONE!!!!!!!!!
-
-        //!!!!!!!Add Assignees NOT DONE!!!!!!!
-
-        //Take a description of the request
+        // Description
         {
           type: 'input',
           block_id: 'desc_block',
@@ -95,8 +129,7 @@ app.command('/hubspot-ticket', async ({ ack, body, client }) => {
   });
 });
 
-
-// --- Modal Submission Handler ---
+// Modal Submission Handler
 app.view('ticket_modal_submit', async ({ ack, body, view, client }) => {
   await ack();
 
@@ -109,71 +142,63 @@ app.view('ticket_modal_submit', async ({ ack, body, view, client }) => {
   const prio = values.prio_block.prio_select.selected_option.value;
   const team = values.team_block.team_select.selected_option.value;
 
-  // --- Create new Notion page ---
-  await notion.pages.create({
-    parent: { data_source_id: process.env.NOTION_DB_ID },
-    properties: {
+  try {
+    // Create new Notion page
+    await notion.pages.create({
+      parent: { database_id: process.env.NOTION_DB_ID },
+      properties: {
+        // Title property
+        Title: {
+          title: [{ text: { content: title } }],
+        },
 
-      'Project name': {
-        rich_text: [{ text: { content: title } }],
+        // Type of ticket
+        Type: {
+          select: { name: type },
+        },
+
+        // Status set to "Not Started"
+        Status: {
+          select: { name: 'Not started' },
+        },
+
+        // Start date
+        'Start Date': {
+          date: { start: getTodayISO() },
+        },
+
+        // Priority
+        Priority: {
+          select: { name: prio },
+        },
+
+        // Team
+        Team: {
+          select: { name: team },
+        },
+
+        // Description
+        Description: {
+          rich_text: [{ text: { content: desc } }],
+        },
       },
+    });
 
-      //Select the type of ticket
-      Type: {
-        select: { name: type },
-      },
-
-      //Status of the ticket is set to "Not Started"
-      Status: {
-        select: { name: 'Not Started' },
-      },
-
-      //Start date of ticket
-      'Start Date': {
-        select: { date: today() }
-      },
-
-      //Select the priority of the ticket (High, Low, Medium)
-      Priority: {
-        select: { name: prio },
-      },
-
-      //Select the assigned team
-      Team: {
-        select: { name: team },
-      },
-
-      //Description of task to be completed
-      Description: {
-        rich_text: [{ text: { content: desc } }],
-      },
-
-      //Created by tag
-      CreatedBy: {
-        rich_text: [{ text: { content: user } }],
-      },
-      
-    },
-    // children: [
-    //   {
-    //     object: 'block',
-    //     type: 'paragraph',
-    //     paragraph: {
-    //       rich_text: [{ text: { content: desc } }],
-    //     },
-    //   },
-    // ],
-  });
-
-    // --- Confirm in Slack ---
-  await client.chat.postMessage({
-    channel: body.user.id,
-    text: `Your *${type}* request "${title}" has been added to the Notion Kanban board.`,
-  });
+    // Confirm in Slack
+    await client.chat.postMessage({
+      channel: body.user.id,
+      text: `Your *${type}* request "${title}" has been added to the Notion database.`,
+    });
+  } catch (error) {
+    console.error('Error creating Notion page:', error);
+    await client.chat.postMessage({
+      channel: body.user.id,
+      text: `Sorry, there was an error creating your ticket: ${error.message}`,
+    });
+  }
 });
 
 (async () => {
-    await app.start(process.env.PORT || 3000);
-    console.log(`Bot app is running on port $(process.env.PORT || 3000})`)
-});
-
+  await app.start(process.env.PORT || 3000);
+  console.log(`Bot app is running on port ${process.env.PORT || 3000}`);
+})();
